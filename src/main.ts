f2df0cbf7e25b71dbf3aaf0b7558de017a739dea
NeoNekooo <lgpplg7@gmail.com>
2025-03-24 14:13:14 +0700

    y

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://poke-dex-rust.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
